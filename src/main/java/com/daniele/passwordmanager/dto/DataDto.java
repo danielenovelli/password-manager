172a97bf49cf01230787e6be823764f625538ad9
daniele.novelli <d.n.danielenovelli@gmail.com>
2023-11-18 13:20:06 +0100

    Refactor dto

--- src/main/java/com/daniele/passwordmanager/dto/DataDto.java
+++ src/main/java/com/daniele/passwordmanager/dto/DataDto.java
@@ -1,14 +1,16 @@
 package com.daniele.passwordmanager.dto;
 
+import com.daniele.passwordmanager.entities.Data;
 import com.daniele.passwordmanager.entities.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,4 +26,59 @@ public class DataDto {
     private String password;
     private String url;
     private String notes;
+
+    public static DataDto toDataDto(Data data) {
+        if (data == null) {
+            return null;
+        }
+        DataDto dataDto = new DataDto();
+        dataDto.setId(data.getId());
+        dataDto.setName(data.getName());
+        dataDto.setEmail(data.getEmail());
+        dataDto.setUsername(data.getUsername());
+        dataDto.setPassword(data.getPassword());
+        dataDto.setUrl(data.getUrl());
+        dataDto.setNotes(data.getNotes());
+
+        return dataDto;
+    }
+
+    public static List<DataDto> toDataDtoList(List<Data> dataList) {
+        if (dataList == null) {
+            return null;
+        }
+        List<DataDto> list = new ArrayList<DataDto>(dataList.size());
+        for (Data data : dataList) {
+            list.add(toDataDto(data));
+        }
+
+        return list;
+    }
+
+    public static Data toDataEntity(DataDto dataDto) {
+        if (dataDto == null) {
+            return null;
+        }
+        Data.DataBuilder data = Data.builder();
+        data.id(dataDto.getId());
+        data.name(dataDto.getName());
+        data.email(dataDto.getEmail());
+        data.username(dataDto.getUsername());
+        data.password(dataDto.getPassword());
+        data.url(dataDto.getUrl());
+        data.notes(dataDto.getNotes());
+
+        return data.build();
+    }
+
+    public static List<Data> toDataEntityList(List<DataDto> dataDtoList) {
+        if (dataDtoList == null) {
+            return null;
+        }
+        List<Data> list = new ArrayList<Data>(dataDtoList.size());
+        for (DataDto dataDto : dataDtoList) {
+            list.add(toDataEntity(dataDto));
+        }
+        return list;
+    }
 }
--- src/main/java/com/daniele/passwordmanager/dto/UserDto.java
+++ src/main/java/com/daniele/passwordmanager/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.daniele.passwordmanager.dto;
 
 import com.daniele.passwordmanager.entities.Data;
+import com.daniele.passwordmanager.entities.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Email;
@@ -10,6 +11,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -30,4 +34,57 @@ public class UserDto {
     private String password;
     private Date dateOfBirth;
     private String role;
+
+    public static User toUserEntity(UserDto userDto) {
+        if ( userDto == null ) {
+            return null;
+        }
+
+        User.UserBuilder user = User.builder();
+
+        user.id( userDto.getId() );
+        user.firstName( userDto.getFirstName() );
+        user.lastName( userDto.getLastName() );
+        user.username( userDto.getUsername() );
+        user.email( userDto.getEmail() );
+        user.password( userDto.getPassword() );
+        if ( userDto.getDateOfBirth() != null ) {
+            user.dateOfBirth( LocalDateTime.ofInstant( userDto.getDateOfBirth().toInstant(), ZoneOffset.UTC ).toLocalDate() );
+        }
+        user.role( userDto.getRole() );
+
+        return user.build();
+    }
+    public static UserDto toUserDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDto userDto = new UserDto();
+
+        userDto.setId( user.getId() );
+        userDto.setFirstName( user.getFirstName() );
+        userDto.setLastName( user.getLastName() );
+        userDto.setUsername( user.getUsername() );
+        userDto.setEmail( user.getEmail() );
+        userDto.setPassword( user.getPassword() );
+        if ( user.getDateOfBirth() != null ) {
+            userDto.setDateOfBirth( Date.from( user.getDateOfBirth().atStartOfDay( ZoneOffset.UTC ).toInstant() ) );
+        }
+        userDto.setRole( user.getRole() );
+
+        return userDto;
+    }
+    public static List<UserDto> toUserDtoList(List<User> userList) {
+        if ( userList == null ) {
+            return null;
+        }
+
+        List<UserDto> list = new ArrayList<UserDto>( userList.size() );
+        for ( User user : userList ) {
+            list.add( toUserDto( user ) );
+        }
+
+        return list;
+    }
 }
